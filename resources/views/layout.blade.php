7955fc68c8cb2cee72dede7aefdba7a2d7cb509c
icmike <miketj93@gmail.com>
2016-06-27 16:23:14 -0700

    Managing CSS and JS

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,16 +1,24 @@
 <!DOCTYPE html>
+
 <html lang = "en">
+
 <head>
+
 	<meta charset="UTF-8">
+
 	<title>Document</title>
+
+	<link rel="stylesheet" href="{{ elixir('css/app.css') }}">
 
 	@yield('header')
 </head>
+
 <body>
 
-@yield('content')
+	@yield('content')
 
-@yield('footer')
+	@yield('footer')
 
 </body>
+
 </html>
